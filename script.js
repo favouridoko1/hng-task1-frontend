2b43cecbddb6132b3e642a82ba869850f9538e3c
Favour Idoko <ojodomofavour2004@gmail.com>
2024-07-03 18:58:40 +0100

    Commiting all changes made

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let time = document.querySelectorAll('[data-testid="currentTimeUTC"]');
-let date = document.querySelectorAll('[data-testid="currentDay"]');
+let currentDayIs = document.querySelector(".dayIs");
+let currentTimeIs = document.querySelector('.timeIs');
 
-// time.innerH = 
+let date = new Date();
+function curentDay() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayOfWeek = daysOfWeek[date.getDay()];
+    return currentDayIs.innerHTML = `Current day: ${currentDayOfWeek}`;
+}
+curentDay();
+
+function currentTime() {
+    // const date = new Date().getUTCDate();
+    return currentTimeIs.innerHTML = `Current time: ${date.getUTCHours() + 1}:${date.getUTCMinutes()}`
+    
+    // console.log(currentTimeIs);
+}
+
+currentTime();
+
